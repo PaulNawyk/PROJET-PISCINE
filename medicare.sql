1de1af85a882a67850023eb206ce757fd84912af
Paul <122023894+PaulNawyk@users.noreply.github.com>
2024-05-30 13:47:49 +0200

    Add files via upload

---
+++ medicare.sql
@@ -0,0 +1,325 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 30, 2024 at 11:46 AM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `medicare`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `disponibilites`
+--
+
+CREATE TABLE `disponibilites` (
+  `id` int(11) NOT NULL,
+  `medecin_id` int(11) NOT NULL,
+  `jour` enum('Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche') NOT NULL,
+  `heure_debut` time NOT NULL,
+  `heure_fin` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `historique_consult`
+--
+
+CREATE TABLE `historique_consult` (
+  `id` int(11) NOT NULL,
+  `medecin_id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `heure` time NOT NULL,
+  `details` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `laboratoire`
+--
+
+CREATE TABLE `laboratoire` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `salle` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `telephone` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `email` varchar(255) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medecin`
+--
+
+CREATE TABLE `medecin` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `photo` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `bureau` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `cv` text CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medecin_spe`
+--
+
+CREATE TABLE `medecin_spe` (
+  `medecin_id` int(11) NOT NULL,
+  `specialite_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rdv`
+--
+
+CREATE TABLE `rdv` (
+  `id` int(11) NOT NULL,
+  `medecin_id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `laboratoire_id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `heure` time NOT NULL,
+  `adresse` varchar(255) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services`
+--
+
+CREATE TABLE `services` (
+  `id` int(11) NOT NULL,
+  `laboratoire_id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services_creneaux`
+--
+
+CREATE TABLE `services_creneaux` (
+  `id` int(11) NOT NULL,
+  `services_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `heure_debut` time NOT NULL,
+  `heure_fin` time NOT NULL,
+  `disponible` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `specialites`
+--
+
+CREATE TABLE `specialites` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `prenom` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `email` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `mdp` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `type` enum('admin','medecin','client') CHARACTER SET latin1 NOT NULL,
+  `telephone` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `adresse_ligne1` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `adresse_ligne2` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `ville` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `codePost` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `pays` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `carte_vitale` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `type_carte` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `numero_carte` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `nom_carte` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `date_expi` date NOT NULL,
+  `code_carte` varchar(255) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `nom`, `prenom`, `email`, `mdp`, `type`, `telephone`, `adresse_ligne1`, `adresse_ligne2`, `ville`, `codePost`, `pays`, `carte_vitale`, `type_carte`, `numero_carte`, `nom_carte`, `date_expi`, `code_carte`) VALUES
+(1, 'Gerin', 'Paul', 'paul.gerin@edu.ece.fr', '$2y$10$qefsfKak0mSxHPdcI4eiou1LArkngINaSLUz3Au7kr7sb93Y8J/fy', 'client', '0638752665', '13 avenue maurice hauriou', '', 'Toulouse', '31000', 'France', '1021020192012', '', '', '', '9999-12-31', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `disponibilites`
+--
+ALTER TABLE `disponibilites`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `historique_consult`
+--
+ALTER TABLE `historique_consult`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `laboratoire`
+--
+ALTER TABLE `laboratoire`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `medecin`
+--
+ALTER TABLE `medecin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rdv`
+--
+ALTER TABLE `rdv`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `services_creneaux`
+--
+ALTER TABLE `services_creneaux`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `specialites`
+--
+ALTER TABLE `specialites`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `disponibilites`
+--
+ALTER TABLE `disponibilites`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `historique_consult`
+--
+ALTER TABLE `historique_consult`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `laboratoire`
+--
+ALTER TABLE `laboratoire`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `medecin`
+--
+ALTER TABLE `medecin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rdv`
+--
+ALTER TABLE `rdv`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `services_creneaux`
+--
+ALTER TABLE `services_creneaux`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `specialites`
+--
+ALTER TABLE `specialites`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
